ae7c0bdee9cb5a21459e095b57516442539fbca6
sonia <pawlaksonia@gmail.com>
2021-01-29 17:36:22 +0100

    Seperate template for error message

--- src/api/views/main.py
+++ src/api/views/main.py
@@ -6,33 +6,25 @@ from api.repository.mongo import save_articles
 import json
 
 
-def get_error_msg_if_exists():
-    err = session.get("error_msg")
-    if err:
-        session["error_msg"] = ""
-        return err
-
-
 def construct_views_blueprint(mongo):
     main = Blueprint("main", __name__)  # initialize blueprint
 
-    @main.route("/")
+    @main.route("/", methods=['GET'])
     def index():
-        err = get_error_msg_if_exists()
-        return render_template("index.html", error_msg=err)
+        return render_template("index.html")
 
     @main.route('/api/news', methods=['GET'])
     def news():
-        if not request.args.get("url"):
-            session["error_msg"] = "Please provide url"
-            return redirect(url_for('.index'))
+        news_url = request.args.get("url")
+        if not news_url:
+            err = "Please provide url"
+            return render_template("error.html", error_msg=err)
         try:
-            news_url = request.args.get("url")
             data = collect_articles(news_url)
             return render_template("news.html", news=data, saved_to_db=False)
-        except Exception as e:
-            session["error_msg"] = e
-            return redirect(url_for('.index'))
+        except NotImplementedError:
+            err = (f"Scrapper for {news_url} not implemented")
+            return render_template("error.html", error_msg=err)
 
     @main.route('/api/news/save', methods=['POST'])
     def save():
@@ -44,4 +36,5 @@ def construct_views_blueprint(mongo):
             logger.error(e)
             user_msg = "Something went wrong"
             return create_response(message=str(user_msg), status=500)
+
     return main
