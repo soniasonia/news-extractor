4fbc16b30699b7e20b4d34ef85a3554c812b92d6
sonia <pawlaksonia@gmail.com>
2021-02-03 16:04:19 +0100

    Remove unnecessary comments

--- src/api/core.py
+++ src/api/core.py
@@ -13,24 +13,9 @@ core_logger = logging.getLogger("core")
 def create_response(
         data: dict = None, status: int = 200, message: str = ""
 ) -> Tuple[Response, int]:
-    """Wraps response in a consistent format throughout the API.
-
-    Format inspired by
-    https://medium.com/@shazow/how-i-design-json-api-responses-71900f00f2db
-    Modifications included:
-    - make success a boolean since there's only 2 values
-    - make message a single string since we will only use
-      one message per response
-    IMPORTANT: data must be a dictionary where:
-    - the key is the name of the type of data
-    - the value is the data itself
-    :param data <str> optional data
-    :param status <int> optional status code, defaults to 200
-    :param message <str> optional message
-    :returns tuple of Flask Response and int
-    """
+    """Wraps response in a consistent format throughout the API."""
     if type(data) is not dict and data is not None:
-        raise TypeError("Data should be a dictionary 😞")
+        raise TypeError("Data should be a dictionary")
 
     response = {"success": 200 <= status < 300,
                 "message": message,
